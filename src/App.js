4a5145428d3cf4f12347fac2aa7ba27981d58758
moon <45509511+moonjh9392@users.noreply.github.com>
2022-08-29 22:45:46 +0900

    마이페이지 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import MyPage from "./components/MyPage";
+
 function App() {
-	return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <MyPage />
+    </div>
+  );
 }
 
 export default App;
