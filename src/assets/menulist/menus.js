30d824ddc6d0d60feb046ac3aea7c2e500c28f3a
moon <45509511+moonjh9392@users.noreply.github.com>
2022-09-02 23:46:17 +0900

    menu 요소 수정

--- src/assets/menulist/menus.js
+++ src/assets/menulist/menus.js
@@ -2,13 +2,11 @@ export const menus = [
   {
     id: 0,
     text: "알람 설정",
-    name: "Alram",
     link: "/mypage/alram",
   },
   {
     id: 1,
     text: "뉴스 보기",
-    name: "News",
     link: "/mypage/news",
   },
 ];
