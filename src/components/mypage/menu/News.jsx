e2d051bcecd72b88fd02a1b241afa909615f4983
moonjh9392 <moonjh9392@gmail.com>
2022-09-06 02:49:39 +0900

    commit

--- src/components/mypage/menu/News.jsx
+++ src/components/mypage/menu/News.jsx
@@ -10,7 +10,7 @@ export default function News() {
 
   const [ref, inView] = useInView();
 
-  const url = '/http/';
+  const url = '/http/naver';
 
   // 서버에서 아이템을 가지고 오는 함수
   const getItems = useCallback(async () => {
