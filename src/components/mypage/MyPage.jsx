027e382669496c9a643bb89bbc722ae3a946e46d
moon <45509511+moonjh9392@users.noreply.github.com>
2022-09-05 21:30:27 +0900

    alram page modify

--- src/components/mypage/MyPage.jsx
+++ src/components/mypage/MyPage.jsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 import Header from "../common/Header";
 
 const MainStyle = styled.section`
+  position: relative;
   margin: 0 auto;
   max-width: 718px;
 
   @media screen and (max-width: 768px) {
     padding: 128px 24px;
-  }
-  h3 {
+    top: -128px;
   }
 `;
 
--- src/components/mypage/menu/Alram.jsx
+++ src/components/mypage/menu/Alram.jsx
@@ -1,71 +1,68 @@
 import styled from "styled-components";
+import ColorBtn from "../../button/ColorBtn";
 
+const Title = styled.h3`
+  border-bottom: 1px solid rgba(226, 228, 232);
+  padding-bottom: 24px;
+  margin-top: 80px;
+  margin-bottom: 48px;
+  font-weight: bold;
+  font-size: 32px;
+  color: rgba(48, 53, 65);
+`;
+const Content = styled.section`
+  .gray {
+    color: rgba(73, 78, 92);
+    font-weight: 500;
+  }
+  .item {
+    margin-bottom: 32px;
+    display: flex;
+    flex-direction: column;
+  }
+  .item-desc {
+    padding: 8px 0;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    border: none;
+    padding: 0 18px;
+    border-radius: 8px;
+    font-size: 18px;
+  }
+  .item-input {
+    border: 1px solid rgba(231, 231, 234);
+    border-radius: 8px;
+    height: 48px;
+    display: flex;
+    justify-content: center;
+  }
+`;
+const Float = styled.div`
+  height: 48px;
+`;
 export default function Alram() {
-  const Title = styled.h3`
-    border-bottom: 1px solid rgba(226, 228, 232);
-    padding-bottom: 24px;
-    margin: 48px 0;
-    font-weight: bold;
-    font-size: 32px;
-    color: rgba(48, 53, 65);
-  `;
-  const Content = styled.section`
-    .float {
-      flex: 0 0 auto;
-      height: 48px;
-    }
-    .gray {
-      color: rgba(73, 78, 92);
-      font-weight: 500;
-    }
-    .input {
-      position: relative;
-      display: flex;
-      align-items: center;
-      width: 100%;
-      height: 48px;
-      margin: 1px auto 0;
-      font-size: 15px;
-      line-height: 20px;
-      color: #333d4b;
-      background-color: #fff;
-      border: 1px solid #333d4b;
-      outline: none;
-      box-shadow: inset 0 0 0 1px rgb(0 27 55 / 10%);
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      appearance: none;
-      overflow: hidden;
-      transition: background 0.2s ease, color 0.1s ease, box-shadow 0.2s ease;
-    }
-    .input__field {
-      width: 100%;
-      height: 100%;
-      min-width: 0;
-    }
-    .radius--m {
-      border-radius: 8px;
-    }
-
-    .input-desc {
-      margin: 24px 0;
-    }
-  `;
   return (
     <>
-      <Title className="gray">알람 설정하기</Title>
+      <Title>알람 설정하기</Title>
       <Content>
         <span className="gray">알람을 받고싶은 지역과 시간을 설정하세요.</span>
-        <div className="float" />
+        <Float />
         <form action="">
-          <div>
-            <label className="input-desc gray">지역 설정</label>
-            <div className="input radius--m input--error">
-              <input className="input__field" type="text" />
+          <div className="item">
+            <label className="item-desc gray">지역 설정</label>
+            <div className="item-input">
+              <input type="text" />
             </div>
           </div>
-          <div className="float" />
-          <button>저장</button>
+          <div className="item">
+            <label className="item-desc gray">시간 설정</label>
+            <div className="item-input">
+              <input type="text" />
+            </div>
+          </div>
+          <ColorBtn text="저장" color="rgba(84,113,241)" fontColor="white" />
         </form>
       </Content>
     </>
