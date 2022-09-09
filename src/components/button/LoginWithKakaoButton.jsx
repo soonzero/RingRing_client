57afbdddcf18ad4a07a842cc44d983790650dd97
soonzero <sykeum0912@naver.com>
2022-09-10 01:25:08 +0900

    :ambulance: Hotfix: 카카오 로그인 기능 수정

--- src/components/button/LoginWithKakaoButton.jsx
+++ src/components/button/LoginWithKakaoButton.jsx
@@ -2,36 +2,36 @@ import styled from 'styled-components';
 import { ReactComponent as KakaoSVG } from '../../assets/icons/kakao.svg';
 
 const LoginWithKakaoBtn = styled.button`
-  display: flex;
-  align-items: center;
-  width: 183px;
-  height: 45px;
-  background-color: #fee500;
-  color: rgba(0, 0, 0, 0.85);
-  border-radius: 6px;
-  padding: 0 11px;
-  margin: 0 auto;
-  cursor: pointer;
+	display: flex;
+	align-items: center;
+	width: 183px;
+	height: 45px;
+	background-color: #fee500;
+	color: rgba(0, 0, 0, 0.85);
+	border-radius: 6px;
+	padding: 0 11px;
+	margin: 0 auto;
+	cursor: pointer;
 
-  span {
-    flex-grow: 1;
-    font-size: 1rem;
-  }
+	span {
+		flex-grow: 1;
+		font-size: 1rem;
+	}
 `;
 
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=http://localhost:3000/kakaologin&response_type=code`;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}/kakaologin&response_type=code`;
 
 const LoginWithKakaoButton = () => {
-  const loginWithKakao = () => {
-    window.location.href = KAKAO_AUTH_URL;
-  };
+	const loginWithKakao = () => {
+		window.location.href = KAKAO_AUTH_URL;
+	};
 
-  return (
-    <LoginWithKakaoBtn onClick={loginWithKakao}>
-      <KakaoSVG />
-      <span>카카오 로그인</span>
-    </LoginWithKakaoBtn>
-  );
+	return (
+		<LoginWithKakaoBtn onClick={loginWithKakao}>
+			<KakaoSVG />
+			<span>카카오 로그인</span>
+		</LoginWithKakaoBtn>
+	);
 };
 
 export default LoginWithKakaoButton;
--- src/routes/KakaoLogin.jsx
+++ src/routes/KakaoLogin.jsx
@@ -3,33 +3,33 @@ import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const KakaoLogin = () => {
-  const navigate = useNavigate();
-  const KAKAO_CODE = useLocation().search.split('=')[1];
+	const navigate = useNavigate();
+	const KAKAO_CODE = useLocation().search.split('=')[1];
 
-  const getToken = async () => {
-    try {
-      const {
-        data: { access_token },
-      } = await axios({
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        url: `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=http://localhost:3000/kakaologin&code=${KAKAO_CODE}`,
-      });
-      // access_token 받고, 서버에 전달해서 JWT 토큰 받기
-      sessionStorage.setItem('token', access_token);
+	const getToken = async () => {
+		try {
+			const {
+				data: { access_token },
+			} = await axios({
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				url: `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}/kakaologin&code=${KAKAO_CODE}`,
+			});
+			// access_token 받고, 서버에 전달해서 JWT 토큰 받기
+			sessionStorage.setItem('token', access_token);
 
-      // 성공하면 다음 페이지로 이동
-      navigate('/');
-    } catch (e) {
-      console.log(e);
-    }
-  };
+			// 성공하면 다음 페이지로 이동
+			navigate('/');
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-  useEffect(() => {
-    getToken();
-  }, []);
+	useEffect(() => {
+		getToken();
+	}, []);
 
-  return <>카카오 로그인 중...</>;
+	return <>카카오 로그인 중...</>;
 };
 
 export default KakaoLogin;
